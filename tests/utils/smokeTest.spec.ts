04f564d4d2200a59ab299586b7c3bb2f9fb52679
YourUsername <farxadahmedo@gmail.com>
2026-06-30 16:35:13 +0400

    create api-test class

---
+++ tests/utils/smokeTest.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+import { RequestHandler } from './request-handler';
+
+test('first test', async ({}) => {
+  const api = new RequestHandler();
+  api.url(https://conduit-api.bondaracademy.com/api/articles');
+
+    
+});
